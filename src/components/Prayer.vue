4ed7543e84d377d66c3deccd62adb8503c765bba
Nuh-local <69435206+Nuh-h@users.noreply.github.com>
2021-12-31 21:47:25 +0000

    1/Rewrite auto-complete. 2/Rewrite prayer times into cards. 3/Add chips to show last 4 cities.

--- src/components/Prayer.vue
+++ src/components/Prayer.vue
@@ -6,35 +6,72 @@
           <img src="../assets/prayer-img.png" class="logo-img" />
         </div>
         <span v-show="requestError" id="error">
-          {{errorMessage}} &#x1F62C;  <!-- &#x1F62C = grimacing face -->
+          {{ errorMessage }} &#x1F62C;
+          <!-- &#x1F62C = grimacing face -->
         </span>
         <div class="searchBar">
-          <b-field >
-            <b-autocomplete
-                rounded
-                v-model="cityInput"
-                :data="matchingCities"
-                placeholder="Enter name of your city and select"
-                icon="magnify"
-                clearable
-                check-infinite-scroll
-                @select=" option => (res = option.split(', '), city=res[0], state=res[1], country=res[2], getPrayerTimes() )"
-                
-                >
-                <template #empty>City not found</template>
-            </b-autocomplete>
+          <b-field>
+            <v-autocomplete
+              v-model="previousCity"
+              :items="matchingCities"
+              :loading="loading"
+              color="blue"
+              :search-input.sync="cityInput"
+              hide-no-data
+              hide-selected
+              item-text="matching_full_name"
+              item-value="matching_full_name"
+              label="Your City:"
+              placeholder="e.g. San Antonio"
+              prepend-icon="mdi-city"
+              dark
+              filled
+              chip
+              dense
+              chips
+              deletable-chips
+              return-object
+            >
+            </v-autocomplete>
           </b-field>
         </div>
-        <div v-if="locationDetermined" >
-          <h1>{{ city }}</h1>
-          <h1>{{ wholeResponse.data.date.gregorian.date}}</h1>
-          <h4>Fajr: {{ wholeResponse.data.timings.Fajr }}</h4>
-          <h4 class="sunrise">Sunrise: {{ this.wholeResponse.data.timings.Sunrise }}</h4>
-          <h4>Dhuhr: {{ wholeResponse.data.timings.Dhuhr }}</h4>
-          <h4>Asr: {{ wholeResponse.data.timings.Asr }}</h4>
-          <h4 class="sunset">Maghrib: {{ wholeResponse.data.timings.Maghrib }}</h4>
-          <h4>Isha: {{ wholeResponse.data.timings.Isha }}</h4>
+        <v-container class="py-0">
+          <v-row align="center" justify="start">
+            <v-col
+              v-for="(selection, i) in selectedCities"
+              :key="selection.city + i"
+              class="shrink"
+            >
+              <v-chip-group
+                :mandatory="true"
+                :column="true"
+                active-class="secondary--text"
+              >
+                <v-chip
+                  :disabled="loading"
+                  close
+                  outlined
+                  :color="selectedCity(selection.city)"
+                  @click:close="selectedCities.splice(i, 1)"
+                  @click="previousCity = selection.city_full_name"
+                >
+                  {{ selection.city }}
+                </v-chip>
+              </v-chip-group>
+            </v-col>
+          </v-row>
+        </v-container>
+        <v-container v-if="locationDetermined">
+        <h1>{{ city }}</h1><h4 style="">{{ wholeResponse.data.date.gregorian.date }}</h4>
+        <div class="prayer__times" >
+          <prayer name="Fajr" :time="wholeResponse.data.timings.Fajr"></prayer>
+          <prayer name="Ishraq" :time="wholeResponse.data.timings.Sunrise"></prayer>
+          <prayer name="Dhuhr" :time="wholeResponse.data.timings.Dhuhr"></prayer>
+          <prayer name="Asr" :time="wholeResponse.data.timings.Asr"></prayer>
+          <prayer name="Maghrib" :time="wholeResponse.data.timings.Maghrib" ></prayer>
+          <prayer name="Isha" :time="wholeResponse.data.timings.Isha"></prayer>
         </div>
+        </v-container>
       </div>
     </v-container>
   </div>
@@ -44,125 +81,168 @@
 /* eslint no-console: "warn" */
 /*eslint no-inner-declarations: "off"*/
 import axios from "axios";
+import prayer from "./PrayerTimeCard.vue";
 
 export default {
-  components: {},
+  components: { prayer },
   data() {
     return {
       wholeResponse: [],
       loading: true,
-      city: '',
-      state: '',
-      country: '',
+      city: "",
+      state: "",
+      country: "",
       locationDetermined: false,
       requestError: false,
-      errorMessage: 'Failed to Get Prayer Times',
+      errorMessage: "Failed to Get Prayer Times",
 
-      cityInput: '',
+      cityInput: "",
       cities: [],
-      previousCity:''
+      previousCity: "",
+      selectedCities: [],
+      // mandatory: false
     };
   },
 
   watch: {
-    // whenever input changes, this function will run
+    // whenever input changes, this function will run to update the autocomplete
     cityInput: function (newCity) {
-      newCity == "" ? this.cities = [] : this.updateCities(newCity);
-    }
+      newCity == "" ? (this.cities = []) : this.updateCities(newCity);
+    },
+    //whenever previous city changes (i.e. new city selected), this function will trigger fetch prayer times
+    previousCity: function (prevCity) {
+      if (!prevCity.includes(", ")) return;
+      const res = prevCity.split(", ");
+      this.city = res[0];
+      this.state = res[1];
+      this.country = res[2];
+      this.getPrayerTimes();
+    },
   },
 
   mounted() {
     // enable checks for query parameters
     // this would allow bookmarking prayer locations
     // e.g. https://host/#/?city=london&country=GB
-    this.$route.query.city ? this.city = this.$route.query.city : '';
-    this.$route.query.state ? this.state = this.$route.query.state : '';
-    this.$route.query.country ? this.country = this.$route.query.country : '';
+    this.$route.query.city ? (this.city = this.$route.query.city) : "";
+    this.$route.query.state ? (this.state = this.$route.query.state) : "";
+    this.$route.query.country ? (this.country = this.$route.query.country) : "";
 
-    if (this.city && this.country) { // min of city and country is required to get prayer times
-      this.getPrayerTimes({ method: 'city-state-country'});
+    if (this.city && this.country) {
+      // min of city and country is required to get prayer times
+      this.getPrayerTimes({ method: "city-state-country" });
     } else if (navigator) {
-        const options = {
-          enableHighAccuracy: true,
-          timeout: 5000,
-          maximumAge: 0
-        };
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
 
-        // Save a ref to `this` because the context changes within success callback
-        const self = this;
+      // Save a ref to `this` because the context changes within success callback
+      const self = this;
 
-        function success(pos) {
-          const crd = pos.coords;
-          const { longitude, latitude } = crd
-          self.getPrayerTimes({ method: 'coordinates', longitude, latitude });
-        }
+      function success(pos) {
+        const crd = pos.coords;
+        const { longitude, latitude } = crd;
+        self.getPrayerTimes({ method: "coordinates", longitude, latitude });
+      }
 
-        function error(err) {
-          console.warn(`ERROR(${err.code}): ${err.message}`);
-        }
-        // request location through the browser (we never store this, just pass it to the API)
-        navigator.geolocation.getCurrentPosition(success, error, options);
+      function error(err) {
+        console.warn(`ERROR(${err.code}): ${err.message}`);
+      }
+      // request location through the browser (we never store this, just pass it to the API)
+      navigator.geolocation.getCurrentPosition(success, error, options);
     }
   },
+
   methods: {
-  async getPrayerTimes (config = { method: 'city-state-country' }) {
-    let response;
-    let apiUrl="https://api.aladhan.com/v1";
-    let queryString = "?"
+    async getPrayerTimes(config = { method: "city-state-country" }) {
+      let response;
+      let apiUrl = "https://api.aladhan.com/v1";
+      let queryString = "?";
 
-    if (config.method === 'coordinates'){
-      queryString += `latitude=${config.latitude}&longitude=${config.longitude}`
-      apiUrl += `/timings${queryString}`
-    } else if (config.method === 'city-state-country') {
-      queryString += `city=${this.city}&state=${this.state}&country=${this.country}`
-      apiUrl += `/timingsByCity${queryString}`
-    }
+      if (config.method === "coordinates") {
+        queryString += `latitude=${config.latitude}&longitude=${config.longitude}`;
+        apiUrl += `/timings${queryString}`;
+      } else if (config.method === "city-state-country") {
+        queryString += `city=${this.city}&state=${this.state}&country=${this.country}`;
+        apiUrl += `/timingsByCity${queryString}`;
+      }
 
-    try {
-      response = await axios.get(`${apiUrl}`);
-      this.wholeResponse = response.data;
-      this.locationDetermined = true;
-      this.requestError = false;
+      try {
+        response = await axios.get(`${apiUrl}`);
+        this.wholeResponse = response.data;
+        this.locationDetermined = true;
+        this.requestError = false;
 
-      this.previousCity = this.city;
-      this.cityInput = '';
+        this.previousCity = this.city;
+        const chip = {
+          city: this.previousCity,
+          city_full_name: this.city + ", " + this.state + ", " + this.country,
+        };
 
-    } catch(error) {
+        //update selected cities, but keep to 4 items
+        if (
+          this.selectedCities
+            .map((item) => item.city_full_name)
+            .indexOf(chip.city_full_name) === -1
+        ) {
+          if (this.selectedCities.length >= 4) this.selectedCities.splice(0, 1);
+          this.selectedCities.push(chip);
+        }
+
+      } catch (error) {
         if (error.response) {
           // The request was made and the server responded with a status code
           // that falls out of the range of 2xx
-          console.error('Error with the response: ', error.response);
+          console.error("Error with the response: ", error.response);
           this.errorMessage = error.response.data.data;
         } else if (error.request) {
           // The request was made but no response was received
           // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
           // http.ClientRequest in node.js
-          console.error('Error with the request:', error.request);
+          console.error("Error with the request:", error.request);
         } else {
           // Something happened in setting up the request that triggered an Error
-          console.error('Error setting up the request:', error.message);
+          console.error("Error setting up the request:", error.message);
         }
-        this.city = this.previousCity; //ensures city matches displayed timetable
         this.requestError = true; // displays error message
       }
     },
 
-    async updateCities(newCity){
-       await axios.get(`https://api.teleport.org/api/cities/?search=${newCity}`)
-        .then((response)=>{this.cities=response.data._embedded[`city:search-results`]})
-        .catch((err)=>{console.log(err)});
-      
-    }
+    async updateCities(newCity) {
+      this.loading = true;
+      await axios
+        .get(`https://api.teleport.org/api/cities/?search=${newCity}`)
+        .then((response) => {
+          this.cities = response.data._embedded[`city:search-results`];
+          this.loading = false;
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    },
+
+    //to colour-code the chips that highlight past cities
+    selectedCity(selection) {
+      return selection != this.city ? "grey" : "blue";
+    },
   },
   computed: {
     matchingCities() {
-      return this.cities.map(c => c.matching_full_name);
-    }
-  }
+      return this.cities.map((c) => c.matching_full_name);
+    },
+  },
 };
 </script>
 
 <style scoped>
+.prayer__times {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+}
 .nav-bar {
   height: 100%;
   position: fixed;
@@ -209,11 +289,11 @@ h1 {
 h4 {
   font-size: 30px;
 }
-input[type=text] {
+input {
   text-align: center;
-  color: white;
+  color: rgb(190, 0, 0);
   margin-bottom: 10px;
-  width:300px;
+  width: 300px;
 }
 span {
   color: gold;
@@ -221,7 +301,7 @@ span {
 }
 
 .searchBar {
-  width:400px;
-  margin:0 auto;
+  width: 400px;
+  margin: 0 auto;
 }
 </style>
---
+++ src/components/PrayerTimeCard.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="prayer" :style=" this.cssVars ">
+    <img alt="prayer icon" class="prayer__logo" :src="`${icons[name]} `" />
+    <p class="prayer__time">{{ time }}</p>
+    <p class="prayer__name">{{name}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "prayer",
+  props: ["name", "time"],
+
+  data() {
+    return {
+      icons: {
+        Fajr: "https://th.bing.com/th/id/OIP.1n1Rv7qNDGqVrc0SLZk9egHaHa?pid=ImgDet&rs=1",
+        Dhuha: "https://cdn0.iconfinder.com/data/icons/weather-406/24/weather-afternoon-day-time-pm-128.png",
+        Ishraq: "https://cdn0.iconfinder.com/data/icons/weather-406/24/weather-afternoon-day-time-pm-128.png",
+        Dhuhr: "https://cdn3.iconfinder.com/data/icons/space-lineal-color-set/512/Sun-512.png",
+        Asr: "https://cdn0.iconfinder.com/data/icons/weather-406/24/morning-day-time-weather-512.png",
+        Maghrib: "https://image.flaticon.com/icons/png/512/856/856329.png",
+        Isha: "https://media.istockphoto.com/vectors/cloudy-night-icon-white-weather-icon-vector-id1131028105?k=6&m=1131028105&s=612x612&w=0&h=vThX-bgrpfTQfkbsKgS7Fp-u12WkJdoeYm1eN_mtiUc=",
+      },
+    };
+  }
+};
+</script>
+
+<style scoped>
+
+.prayer {
+  display: flex;
+  flex-direction: row;
+  margin: 0 auto;
+  background: rgba(11, 4, 114, 0.815);
+  width: 50vh;
+  border-radius: 12px;
+  overflow: hidden;
+  margin: 2%;
+  position: relative;
+}
+.prayer__logo {
+  width: 100px;
+  border-radius: 12px 0 0 12px;
+  background: darkcyan;
+}
+.prayer__time {
+  width: 100%;
+  font-size: 60px;
+  text-align: center;
+  width: 100%;
+  align-self: center;
+  padding: 0%;
+  margin: 0;
+}
+.prayer__name {
+    display: block;
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    color:orange;
+    padding: 2%;
+    text-align:right
+    /* background:rebeccapurple; */
+}
+</style>
