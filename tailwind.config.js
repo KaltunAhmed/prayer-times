1ce27097644c472668984ecfb9dc07f57a7c8937
Nuh-local <69435206+Nuh-h@users.noreply.github.com>
2022-04-18 01:55:09 +0100

    [+] Refactors and adds initial base design

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      ["theme-0"]: "#03045E",
+      ['theme-1']: "#023E8A",
+      ['theme-2']: "#0077B6",
+      ['theme-3']: "#0096C7",
+      ['theme-4']: "#00B4D8",
+      ['theme-5']: "#48CAE4",
+      ['theme-6']: "#90E0EF",
+      ['theme-7']: "#ADE8F4",
+      ['theme-8']: "#CAF0F8",
+    },
     extend: {},
   },
   plugins: [],
